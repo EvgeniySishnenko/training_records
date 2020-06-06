406a1921b11c9eb73fa856c8d1a20c832af05e72
“Евгений <sishnenko86@mail.ru>
2020-06-06 23:49:48 +0300

    первый комит

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import Item from "./Components/trainingRecords/Item";
+// import moment from "moment";
+import { nanoid } from "nanoid";
+import { findLastIndex } from "lodash";
 function App() {
+  const [input, setInput] = useState([{}]);
+  const [training, setTrainig] = useState([]);
+  const valueForm = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  function removeTraining(id) {
+    setTrainig(training.filter((training) => training.id !== id));
+  }
+  const handleForm = (e) => {
+    e.preventDefault();
+    setTrainig(
+      training
+        .concat([
+          {
+            id: nanoid(),
+            date: input.date,
+            passed: input.passed,
+          },
+        ])
+        .sort()
+        .reverse()
+    );
+    // e.target.passed.value = "";
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <form onSubmit={handleForm}>
+          <label htmlFor="date">
+            <span className="heading-date">Дата</span>
+            <input
+              name="date"
+              type="date"
+              // onChange={(e) => setInput(e.target.value)}
+              onChange={valueForm}
+              value={input.date}
+            />
+          </label>
+          <label htmlFor="passed">
+            <span className="heading-passed">Пройдено</span>
+            <input
+              name="passed"
+              type="number"
+              // onChange={(e) => setInput(e.target.value)}
+              onChange={valueForm}
+              value={input.passed}
+            />
+          </label>
+          {/* <input type="submit" onClick={handleBtn} value="OK" /> */}
+          <button type="submit"> OK </button>
+        </form>
+        <div className="result">
+          <div className="heading">
+            <div className="heading-date">Дата</div>
+            <div className="heading-passed">Пройдено</div>
+            <div className="heading-act">Действия</div>
+          </div>
+          <div className="list">
+            {training.map((a) => (
+              <Item key={a.id} training={a} onRemove={removeTraining} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/trainingRecords/Item.js
@@ -0,0 +1,22 @@
+import React from "react";
+function Item(props) {
+  const { training } = props;
+  const onRemove = (e) => {
+    props.onRemove(e.target.dataset.id);
+  };
+
+  return (
+    <div className="item">
+      <div className="item-date">{training.date}</div>
+      <div className="item-passed">{training.passed}</div>
+      <div className="item-act">
+        {/* <span className="edit">Edit</span> */}
+        <span onClick={onRemove} className="del" data-id={training.id}>
+          Del
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Item;
